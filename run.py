8618bba05323eac68d489c2a19471b8b4b8444ed
chazzieloves <cassandra.olandersson@gmail.com>
2023-09-26 01:11:03 +0200

    Add main loop

--- run.py
+++ run.py
@@ -24,3 +24,67 @@ def display_board(board, revealed):
             else:
                 print("?", end=" ")
         print()
+
+
+def is_winner(revealed):
+    """Check if player won."""
+    for line in revealed:
+        if False in line:
+            return False
+    return True
+
+
+def main():
+    revealed = [[False] * GRID_SIZE for _ in range(GRID_SIZE)]
+    tries = 0
+    score = 0
+
+    while not is_winner(revealed):
+        display_board(BOARD, revealed)
+        print(f"Tries: {tries} Score: {score}")
+
+        # Get input from user on first card.
+        while True:
+            try:
+                row1, col1 = map(int, input("Enter row and column: ").split())
+                if (
+                    0 <= row1 < GRID_SIZE
+                    and 0 <= col1 < GRID_SIZE
+                    and not revealed[row1][col1]
+                ):
+                    break
+                else:
+                    print("Try again.")
+            except ValueError:
+                print("Try again.")
+
+        # Get input from user on second card.
+        while True:
+            try:
+                row2, col2 = map(int, input("Enter row and column: ").split())
+                if (
+                    0 <= row2 < GRID_SIZE
+                    and 0 <= col2 < GRID_SIZE
+                    and (row1 != row2 or col1 != col2)
+                    and not revealed[row2][col2]
+                ):
+                    break
+                else:
+                    print("Invalid input, try again.")
+            except ValueError:
+                print("Invalid input, try again.")
+
+        # Reveal selected cards.
+        tries += 1
+        if BOARD[row1][col1] == BOARD[row2][col2]:
+            print("It´s a match!")
+            revealed[row1][col1] = revealed[row2][col2] = True
+            score += 1
+        else:
+            print("Not a match. Try again!")
+
+    print("Yeeey! You won!")
+    print(f"Total Tries: {tries} Total Score: {score}")
+
+    if __name__ == "__main__":
+        main()

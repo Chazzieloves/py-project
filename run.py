a5ac28fc241c0a27ccc24101ad1f2a8630165f09
chazzieloves <cassandra.olandersson@gmail.com>
2023-09-26 00:25:25 +0200

    display board game

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-"""Create memory game"""
+"""Create memory game."""
 
 import random
 
@@ -6,8 +6,21 @@ GRID_SIZE = 4
 SYMBOLS = "ABCDEFGHIJKLMNOPQRST"
 CARDS = random.sample(SYMBOLS, GRID_SIZE * GRID_SIZE // 2) * 2
 random.shuffle(CARDS)
-board = [[None] * GRID_SIZE for _ in range(GRID_SIZE)]
+BOARD = [[None] * GRID_SIZE for _ in range(GRID_SIZE)]
 
 for row in range(GRID_SIZE):
     for col in range(GRID_SIZE):
-        board[row][col] = CARDS.pop()
+        BOARD[row][col] = CARDS.pop()
+
+# Display cards on board. They are hidden from start.
+
+
+def display_board(board, revealed):
+    """Display board."""
+    for line in range(GRID_SIZE):
+        for card in range(GRID_SIZE):
+            if not revealed[line][card]:
+                print(board[line][card], end=" ")
+            else:
+                print("?", end=" ")
+        print()
